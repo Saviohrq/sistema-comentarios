9dcae595932e3bb9dfc2b35ebe9c6244cd9dcdf7
Savio Henrique <saviobarradas@gmail.com>
2025-10-22 03:54:00 -0300

    criação component FormTodo e feita sua comunicação com o componet Comments

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,22 +1,11 @@
 <script setup>
+import  FormTodo  from './FormTodo.vue';
 import { ref, computed, watch } from 'vue';
 
 const comments = ref([]);
-const name = ref('');
-const message = ref('');
 
-function addComment() {
-    if (message.value.trim() === '') {
-        name.value = '';
-        return;
-    }
-    comments.value.push({
-        name: name.value,
-        message: message.value
-    });
-
-    name.value = '';
-    message.value = '';
+function addComment(comment) {
+    comments.value.push(comment);
 }
 
 function removeComment(index) {
@@ -42,15 +31,7 @@ watch(comments, (val) => {
     <div class="container">
         <h1>Comentários</h1>
         <hr />
-        <div class="form-todo form-group">
-            <p>
-                <input placeholder="Nome" type="text" name="author" v-model="name">
-            </p>
-            <p>
-                <textarea placeholder="Comentário" name="message" class="form-control" v-model="message"></textarea>
-            </p>
-            <button @click="addComment" type="submit" class="btn btn-primary">Comentar</button>
-        </div>
+        <FormTodo @add-todo="addComment"></FormTodo>
         <div class="list-group">
             <div class="list-group-item" v-for="(comment, index) in allComments">
                 <span class="comment_author">Autor: <strong>{{ comment.name }}</strong></span>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const message = ref('');
+
+const emit = defineEmits(['add-todo']);
+
+function addComment() {
+    if (message.value.trim() === '') {
+        name.value = '';
+        return;
+    }
+    
+    emit('add-todo', {
+        name: name.value,
+        message: message.value
+    });
+
+    name.value = '';
+    message.value = '';
+}
+</script>
+
+<template>
+    <div class="form-todo form-group">
+        <p>
+            <input placeholder="Nome" type="text" name="author" v-model="name">
+        </p>
+        <p>
+            <textarea placeholder="Comentário" name="message" class="form-control" v-model="message"></textarea>
+        </p>
+        <button @click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+    </div>
+</template>
